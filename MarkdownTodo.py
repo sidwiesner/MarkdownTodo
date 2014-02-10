6f1a279ac712b1f54abff7e151e3481e144f3c1d
Sid Wiesner <swiesner@wellbridgehealth.com>
2014-02-10 16:44:39 -0500

    Working archive command that scans full open file and moves lines to archive file. This still has some warts, but the basics work.

--- MarkdownTodo.py
+++ MarkdownTodo.py
@@ -1,33 +1,57 @@
-import sublime, sublime_plugin
-from datetime import datetime
+import datetime
+import os
+import sublime
+import sublime_plugin
 
 class MarkdownTodoBase(sublime_plugin.TextCommand):
+    """Base class for Markdown Todo actions"""
+    def configure(self):
+        self.settings = sublime.load_settings('MarkdownTodo.sublime-settings')
+        self.archive_file = self.get_filename("archive_file")
+        self.todo_file    = self.get_filename("todo_file")
+        self.waiting_file = self.get_filename("waiting_file")
+        self.someday_file = self.get_filename("someday_file")
+
+    def get_filename(self, key):
+        return os.path.join(self.find_root_directory(), self.settings.get(key))
+
+    def find_root_directory(self):
+        # FIXME: this is too simplistic. Should do a downward directory search
+        # done to make sure this is actually the right directory to use.
+        return os.path.dirname(self.view.file_name())
+
     def run(self, edit):
-        filename = self.view.file_name()
-        # list of allowed filetypes
+        self.configure()
+        file_name = self.view.file_name()
         allowed_filetypes = ('.md', '.markdown', '.mdown')
-        if filename is None or not filename.endswith(allowed_filetypes):
+        if file_name is None or not file_name.endswith(allowed_filetypes):
             return False
         self.runCommand(edit)
 
 class MarkdownTodoAddCommand(MarkdownTodoBase):
-    # FIXME - this isn't implemented
+    """Description"""
     def runCommand(self, edit):
+        # FIXME - this isn't implemented
         for region in self.view.sel():
             lines = self.view.lines(region)
             lines.reverse()
             for line in lines:
-                # don't add a newline when creating new item with cursor is at an empty line
+                # don't add a newline when creating new item with cursor is at
+                # an empty line
                 if not line:
                     line_contents = '- '
                     self.view.insert(edit, line.begin(), line_contents)
-                # add a newline when creating new item when cursor is at another line
+                # add a newline when creating new item when cursor is at another
+                # line
                 else:
                     line_contents = self.view.substr(line) + '\n- '
                     self.view.replace(edit, line, line_contents)
 
 class MarkdownTodoDoneCommand(MarkdownTodoBase):
+    """Mark item as done, prepending the @done and current date/time. Can also
+       be used to reverse its own effects."""
     def runCommand(self, edit):
+        # Only pay attention to current selection
         for region in self.view.sel():
             lines = self.view.lines(region)
             lines.reverse()
@@ -36,34 +60,39 @@ class MarkdownTodoDoneCommand(MarkdownTodoBase):
                 line_contents = self.view.substr(line).strip()
                 # prepend @done if item is ongoing
                 if line_contents.startswith('-'):
-                    self.view.replace(edit, line_head, "+ @done (%s)" % datetime.now().strftime("%Y-%m-%d %H:%M"))
+                    self.view.replace(edit, line_head, "+ @done (%s)" %
+                        datetime.datetime.now().strftime("%Y-%m-%d %H:%M"))
                 # undo @done
                 elif line_contents.startswith('+'):
-                    subfix = self.view.find('(\s)*@done \([^)]+\)', line.begin())
+                    subfix = self.view.find('\s*@done \([^)]+\)', line.begin())
                     self.view.erase(edit, subfix)
                     self.view.replace(edit, line_head, "-")
 
 class MarkdownTodoArchiveCommand(MarkdownTodoBase):
+    """Move done items to the archive file."""
     def runCommand(self, edit):
-        for region in self.view.sel():
-            lines = self.view.lines(region)
-            lines.reverse()
-            for line in lines:
-                line_head = self.view.find("[-\+]", line.begin())
+        # FIXME - should be looking only at specific files?
+        archive_lines = self.view.find_all('\s*\+ @done.+$')
+        archive_lines.reverse() # reverse because it is destructive change
+        # print ("Matches =", len(archive_lines))
+        with open(self.archive_file, "a") as archive_file:
+            for line in archive_lines:
+                # print ("Region found =", line)
                 line_contents = self.view.substr(line).strip()
-                if line_contents.startswith('+'):
-                    if line_contents.find('(\s)*@done(.)+\)$', line.begin()):
-                        # FIXME: line deleted from current file, but also need to delete newline,
-                        # and need to append line to archive.md
-                        subfix = self.view.find('(\s)*@done(.)+\)$', line.begin())
-                        self.view.erase(edit, line)
+                # FIXME: handle any type of line endings
+                # self.view.line_endings()
+                archive_file.write(line_contents + "\n")
+                # FIXME: delete new line from old file too
+                self.view.erase(edit, line)
 
 class MarkdownTodoWaitCommand(MarkdownTodoBase):
+    """Description"""
     # FIXME - this isn't implemented
     def runCommand(self, edit):
         pass
 
 class MarkdownTodoSomedayCommand(MarkdownTodoBase):
+    """Description"""
     # FIXME - this isn't implemented
     def runCommand(self, edit):
         pass
